18da0d86792a2da803b46f9643bc803bb85d7cb3
StepanPletenev <144107101+StepanPletenev@users.noreply.github.com>
2023-11-28 12:57:57 +0300

    Add files via upload

---
+++ dr2-2.pas
@@ -0,0 +1,22 @@
+﻿program dr2_2;
+var
+  s, r: string;
+  l: Integer;
+  p: Boolean;
+
+begin
+  readln(s);
+  for l := Length(s) downto 1 do
+  begin
+    r := r + s[i];
+  end;
+  if s = r then
+    p := True
+  else
+    p:= False;
+  
+  if p then
+    writeln('Строка является палиндромом.')
+  else
+    writeln('Строка не является палиндромом.');
+end.
