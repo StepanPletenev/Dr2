f23a642a88c197bfc5a8835b96b801e23987d2bb
StepanPletenev <144107101+StepanPletenev@users.noreply.github.com>
2023-12-04 08:35:15 +0300

    Add files via upload

---
+++ dr2-1.pas
@@ -0,0 +1,18 @@
+﻿program dr2_1;
+var
+  a: array[1..10] of integer;
+  i, sum: integer;
+
+begin
+  sum := 0;
+  for i := 1 to 10 do
+  begin
+    readln(a[i]); 
+    writeln('a[', i, '] = ', a[i]);
+  end;
+  
+  for i := 2 to 10 do
+    if i mod 2 =0 then
+    sum := sum + a[i];
+  writeln('Сумма элементов на четных позициях: ', sum);
+end.
